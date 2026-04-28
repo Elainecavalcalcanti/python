0106523b48ac5fbd4f51f0a166f99175ae82c3e2
Elainecavalcalcanti <elainepcavalcanti@gmail.com>
2026-04-28 20:52:15 -0300

    exercicio110

---
+++ ex110/moeda.py
@@ -0,0 +1,28 @@
+def aumentar(preço=0, taxa=0, formato=False):
+    resp = preço + (preço * taxa / 100)
+    return resp if formato is False else moeda(resp)
+
+
+def diminuir(preço=0, taxa=0, formato=False):
+    resp = preço - (preço * taxa / 100)
+    return resp if formato is False else moeda(resp)
+
+
+def dobro(preço=0, formato=False):
+    resp = preço * 2
+    return resp if formato is False else moeda(resp)
+
+
+def metade(preço=0, formato=False):
+    resp = preço / 2
+    return resp if formato is False else moeda(resp)
+
+
+def moeda(preço=0, moeda= 'R$'):
+    return f'{moeda}{preço:>.2f}'.replace('.',',')
+
+
+def resumo(preç=0, taxaa=10, taxar=5):
+    print('-'*30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-'*30)
